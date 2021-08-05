05874272d3c0c19bebfffeadf84f5f9f0f3fa4de
github username <github email address>
2021-08-05 09:23:57 -0500

    test data-seed/install webpacker

--- app/controllers/physicians_controller.rb
+++ app/controllers/physicians_controller.rb
@@ -1,5 +1,34 @@
 class PhysiciansController < ApplicationController 
     def index  
-       
+        @physicians = Physician.all
     end
+
+    def show
+        @physician= Physician.find_by_id(params[:id])
+        redirect_to to physician_path if !@physician
+    end
+
+    def new 
+        @physician = Physician.new
+    end
+
+    def create 
+        @physician = Physician.new(physician_params)
+      
+        if @physician.save
+            redirect_to physicians_path
+        else
+            render :new
+        end
+    end
+
+    private 
+
+    def physician_params
+        params.require(:physician).permit(:name, :email)
+    end
+    
+    
+
+       
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  #  get '/physicians', to: 'physicians#index'
+  # get '/physician/:id', to: 'physician#show', as: 'physician'
+   
+  resources :physicians, only: [:new, :show, :index, :create]
+  # resources :Appointement do 
+  #   resources :shoes, shallow: true 
+
+   
+  
 end
